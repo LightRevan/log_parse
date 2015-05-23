544a70e77eaa6b82aa82aad71ace10370b25af6a
LightRevan <lightrevan@yandex.ru>
2015-05-23 10:31:47 +0300

    less basic idea

--- file_parsers.py
+++ file_parsers.py
@@ -43,20 +43,33 @@ class ContextFileParser(BaseFileParser):
         super(ContextFileParser, self).__init__(self, *args, **kwargs)
 
     def next(self):
+        pending_rows = 0
+
         while True:
-            row = self._file.next()
-            row_parse_result = self._row_parser.parse_row(self.row)
-            timestamp = row_parse_result['timestamp']
+            try:
+                row = self._file.next()
+                row_parse_result = self._row_parser.parse_row(self.row)
+                timestamp = row_parse_result['timestamp']
 
-            if self._pattern.find(self.row):
-                pass
-            elif len(self._buffer) == self._context_size:
-                self._buffer.popleft()
+                if self._pattern.find(self.row):
+                    pending_rows = len(self._buffer) + self._context_size
+                elif len(self._buffer) == self._context_size and pending_rows == 0:
+                    self._buffer.popleft()
 
-            self._buffer.append((timestamp, row))
+                self._buffer.append((timestamp, row))
+            except StopIteration:
+                if pending_rows:
+                    pass  # file ended - fuck it, we have buffer
+                else:
+                    raise StopIteration
 
-            self.timestamp, self.row = self._buffer.popleft()
-            return self
+            if pending_rows:
+                try:
+                    self.timestamp, self.row = self._buffer.popleft()
+                    pending_rows -= 1
+                    return self
+                except IndexError:
+                    raise StopIteration  # buffer ended - time to go
 
 
 
