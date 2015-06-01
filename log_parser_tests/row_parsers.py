73eb9490dcdf52e8860cfdb4b0043cab45eccafb
lightrevan <lightrevan@yandex.ru>
2015-06-01 17:27:32 +0300

    implemented unified tests for merging getter and parsers

--- log_parser_tests/row_parsers.py
+++ log_parser_tests/row_parsers.py
@@ -4,19 +4,13 @@ __author__ = 'lightrevan'
 import unittest
 from log_parser.row_parsers import *
 
-# TODO: add full test for all combinations of row parsers and row getters
-# TODO: add tests for match not in first row
 
-
-class MergingGetterSingleParserTestCase(unittest.TestCase):
-    def setUp(self):
-        self.row_parser = SinglePatternRowParser('a', '^(?P<timestamp>\d+) (?P<thread>T\d+)')
-
+class MergingGetterAbstractParserTestCase(object):
     def test_1(self):
         rows = ['1 T1 a',
                 '2 T1 b',
                 '3 T1 a']
-        tested = MergingRowGetter(iter(rows), self.row_parser)
+        tested = self.getter_cls(iter(rows), self.row_parser)
 
         result = [row for row in tested]
         required_result = [('1 T1 a', {'timestamp': '1', 'thread': 'T1', 'match': 'a'}),
@@ -27,59 +21,71 @@ class MergingGetterSingleParserTestCase(unittest.TestCase):
     def test_2(self):
         rows = ['bbb',
                 '1 T1 a',
-                '2 T1 a',
+                '2 T1 b',
                 'cdf',
                 '3 T1 a']
-        tested = MergingRowGetter(iter(rows), self.row_parser)
+        tested = self.getter_cls(iter(rows), self.row_parser)
 
-        result = [row for row, _ in tested]
-        self.assertEqual(result, ['bbb\n1 T1 a', '2 T1 a\ncdf', '3 T1 a'])
+        result = [row for row in tested]
+        required_result = [('bbb\n1 T1 a', {'timestamp': '1', 'thread': 'T1', 'match': 'a'}),
+                           ('2 T1 b\ncdf', {'timestamp': '2', 'thread': 'T1', 'match': None}),
+                           ('3 T1 a', {'timestamp': '3', 'thread': 'T1', 'match': 'a'})]
+        self.assertEqual(result, required_result)
 
     def test_3(self):
         rows = ['bbb',
                 '1 T1 a',
                 'cdf',
-                '3 T1 a']
-        tested = MergingRowGetter(iter(rows), self.row_parser)
+                '3 T1 b']
+        tested = self.getter_cls(iter(rows), self.row_parser)
 
-        result = [row for row, _ in tested]
-        self.assertEqual(result, ['bbb\n1 T1 a\ncdf', '3 T1 a'])
+        result = [row for row in tested]
+        required_result = [('bbb\n1 T1 a\ncdf', {'timestamp': '1', 'thread': 'T1', 'match': 'a'}),
+                           ('3 T1 b', {'timestamp': '3', 'thread': 'T1', 'match': None})]
+        self.assertEqual(result, required_result)
 
     def test_4(self):
         rows = ['1 T1 a',
                 'cdf',
                 '2 T1 b',
-                'acdf']
-        tested = MergingRowGetter(iter(rows), self.row_parser)
-
-        result = [row for row, _ in tested]
-        self.assertEqual(result, ['1 T1 a\ncdf', '2 T1 b\nacdf'])
-
-class MergingGetterMultiParserTestCase(unittest.TestCase):
-    def setUp(self):
-        self.row_parser = MultiPatternRowParser('a', timestamp='^\d+', thread='T\d+')
-
-    def test_1(self):
-        rows = ['1 T1 a',
-                '2 T1 b',
-                '3 T1 a']
-        tested = MergingRowGetter(iter(rows), self.row_parser)
+                'cdf']
+        tested = self.getter_cls(iter(rows), self.row_parser)
 
         result = [row for row in tested]
-        required_result = [('1 T1 a', {'timestamp': '1', 'thread': 'T1', 'match': 'a'}),
-                           ('2 T1 b', {'timestamp': '2', 'thread': 'T1', 'match': None}),
-                           ('3 T1 a', {'timestamp': '3', 'thread': 'T1', 'match': 'a'})]
+        required_result = [('1 T1 a\ncdf', {'timestamp': '1', 'thread': 'T1', 'match': 'a'}),
+                           ('2 T1 b\ncdf', {'timestamp': '2', 'thread': 'T1', 'match': None})]
         self.assertEqual(result, required_result)
 
-    def test_2(self):
-        rows = ['1 T1 a',
-                '2 T1 a',
+    def test_5(self):
+        rows = ['1 T1 b',
                 'cdf',
-                '3 T1 a']
-        tested = MergingRowGetter(iter(rows), self.row_parser)
+                'aaa']
+        tested = self.getter_cls(iter(rows), self.row_parser)
 
-        result = [row for row, _ in tested]
-        self.assertEqual(result, ['1 T1 a', '2 T1 a\ncdf', '3 T1 a'])
+        result = [row for row in tested]
+        required_result = [('1 T1 b\ncdf\naaa', {'timestamp': '1', 'thread': 'T1', 'match': 'a'})]
+        self.assertEqual(result, required_result)
+
+    def test_6(self):
+        rows = ['bbb',
+                'aaa',
+                '1 T1 b']
+        tested = self.getter_cls(iter(rows), self.row_parser)
+
+        result = [row for row in tested]
+        required_result = [('bbb\naaa\n1 T1 b', {'timestamp': '1', 'thread': 'T1', 'match': 'a'})]
+        self.assertEqual(result, required_result)
+
+class MergingGetterSingleParserTestCase(unittest.TestCase, MergingGetterAbstractParserTestCase):
+    def setUp(self):
+        self.row_parser = SinglePatternRowParser('a', '^(?P<timestamp>\d+) (?P<thread>T\d+)')
+        self.getter_cls = MergingRowGetter
+
+
+class MergingGetterMultiParserTestCase(unittest.TestCase, MergingGetterAbstractParserTestCase):
+    def setUp(self):
+        self.row_parser = MultiPatternRowParser('a', timestamp='^\d+', thread='T\d+')
+        self.getter_cls = MergingRowGetter
 
 
 class SinglePatternParserTestCase(unittest.TestCase):
